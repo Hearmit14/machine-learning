684b5ab21a96c181d937d63390e3c39c6046ed7e
Hearmit14 <hejinyang14@gmail.com>
2020-01-10 12:05:36 +0800

    更新部分ffm学习代码

--- code/numpy.py
+++ code/numpy.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 test1 = np.array([[5, 10, 15],
                   [20, 25, 30],
@@ -54,3 +56,170 @@ test3 = test.reshape((-1, 2))
 
 np.arange(10, 30, 5)
 np.arange(12).reshape(3, 4)
+
+
+s = pd.Series([1, 3, 4, np.nan, 7, 9])
+t = pd.Series(s, index=[1, 1, 2, 2, 'a', 4])
+
+# 新建对象 Object Creation
+# 通过传入一个numpy的数组、指定一个时间的索引以及一个列名。
+dates = pd.date_range('20190101', periods=6)
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+df2 = pd.DataFrame(np.arange(12).reshape(3, 4))
+
+
+# 查看 Viewing Data
+df.head()
+df.tail(3)
+df.index
+df.columns
+df2.columns
+
+df.describe()
+df.info()
+
+df.sort_index(axis=1)
+df.sort_index(axis=1, ascending=False)
+df.sort_values(by='B')
+df.sort_values(by='B', ascending=False)
+
+
+# 筛选 Selection
+# 获取某列
+df['A']
+# 选择多行
+df[0:3]
+
+# 选择某行,通过标签选择
+df.loc[dates[0]]
+# 选择指定行列的数据
+df.loc[:, ('A', 'C')]
+df.loc['20190102':'20190105', ('A', 'C')]
+
+# 选择某行,通过位置选择
+df.iloc[3]
+# 选择指定行列的数据
+df.iloc[3:5, 0:2]
+
+# 按条件判断选择
+df[df.A > 0]
+df[df > 0]
+
+
+# 空值处理 Missing Data
+df1 = df.reindex(index=dates[0:4], columns=list(df.columns) + ['E'])
+df1.loc[dates[0]:dates[1], 'E'] = 1
+df1.dropna(how='any')
+df1.fillna(value=5)
+
+
+# 运算 Operations
+# 注意 所有的统计默认是不包含空值的
+df.mean()
+
+# 这里将s的值移动两个，那么空出的部分会自动使用NaN填充
+s = pd.Series([1, 3, 5, np.nan, 6, 8], index=dates)
+s = s.shift(2)
+
+# 通过apply()方法，可以对数据进行逐一操作
+# 累计求和
+df.apply(np.cumsum)
+# 自定义方法
+df.apply(lambda x: x.max() - x.min())
+
+df['A'].value_counts()
+
+
+# String方法
+s = pd.Series(['A', 'B', 'C', 'Aaba', 'Baca', np.nan, 'CABA', 'dog', 'cat'])
+s.str.upper()
+
+s = pd.Series(['A,b', 'c,d'])
+s.str.split(',', expand=True)
+
+
+# 合并 Merge
+df = pd.DataFrame(np.random.randn(10, 4))
+
+# Concat方法
+pieces = [df[:3], df[3:7], df[7:]]
+pd.concat(pieces)
+
+# Merge方法
+left = pd.DataFrame({'key': ['foo', 'foo'], 'lval': [1, 2]})
+right = pd.DataFrame({'key': ['foo', 'foo'], 'rval': [4, 5]})
+pd.merge(left, right, on='key')
+
+# Append方法
+df = pd.DataFrame(np.random.randn(8, 4), columns=['A', 'B', 'C', 'D'])
+s = df.iloc[3]
+df.append(s, ignore_index=True)
+
+
+# 分组 Grouping
+df = pd.DataFrame({'A': ['foo', 'bar', 'foo', 'bar',
+                         'foo', 'bar', 'foo', 'foo'],
+                   'B': ['one', 'one', 'two', 'three',
+                         'two', 'two', 'one', 'three'],
+                   'C': np.random.randn(8),
+                   'D': np.random.randn(8)})
+
+df.groupby('A').sum()
+df.groupby(['A', 'B']).sum()
+
+
+# 整形 Reshaping
+# 堆叠 Stack,python的zip函数可以将对象中对应的元素打包成一个个的元组
+tuples = list(zip(['bar', 'bar', 'baz', 'baz',
+                   'foo', 'foo', 'qux', 'qux'],
+                  ['one', 'two', 'one', 'two',
+                   'one', 'two', 'one', 'two']))
+
+tuples1 = np.array([['bar', 'bar', 'baz', 'baz',
+                     'foo', 'foo', 'qux', 'qux'],
+                    ['one', 'two', 'one', 'two',
+                     'one', 'two', 'one', 'two']])
+
+index = pd.MultiIndex.from_tuples(tuples, names=['first', 'second'])
+
+# 分类目录类型 Categoricals
+# 类型转换：astype('category')
+df = pd.DataFrame({"id": [1, 2, 3, 4, 5, 6],
+                   "raw_grade": ['a', 'b', 'b', 'a', 'a', 'e']})
+
+df['grade'] = df['raw_grade'].astype('category')
+df['grade']
+# 重命名分类：cat
+df["grade"].cat.categories = ["very good", "good", "very bad"]
+df['grade']
+# 重分类：
+df['grade'] = df['grade'].cat.set_categories(
+    ["very bad", "bad", "medium", "good", "very good"])
+df['grade']
+# 排列
+df.sort_values(by="grade")
+# 分组
+df.groupby("grade").size()
+
+# 画图 Plotting
+# Series画图
+ts = pd.Series(np.random.randn(1000),
+               index=pd.date_range('1/1/2019', periods=1000))
+ts = ts.cumsum()
+
+ts.plot()
+plt.show()
+
+# DataFrame画图
+df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index,
+                  columns=['A', 'B', 'C', 'D'])
+df = df.cumsum()
+plt.figure()
+df.plot()
+plt.legend(loc='best')
+plt.show()
+
+
+# 导入导出数据 Getting Data In/Out
+df.to_csv('foo.csv')
+pd.read_csv('foo.csv')
---
+++ pratice/loan/xlearn_ffm.py
@@ -0,0 +1,117 @@
+from sklearn.feature_extraction import DictVectorizer
+import pandas as pd
+import xlearn as xl
+
+train = pd.read_csv(
+    '/Users/hejinyang/Desktop/machine_learning/pratice/loan/train_ctrUa4K.csv')
+
+test = pd.read_csv(
+    '/Users/hejinyang/Desktop/machine_learning/pratice/loan/test_lAUu6dG.csv')
+
+
+train.info()
+# Loan_ID              614 non-null object
+# Gender               601 non-null object
+# Married              611 non-null object
+# Dependents           599 non-null object
+# Education            614 non-null object
+# Self_Employed        582 non-null object
+# ApplicantIncome      614 non-null int64
+# CoapplicantIncome    614 non-null float64
+# LoanAmount           592 non-null float64
+# Loan_Amount_Term     600 non-null float64
+# Credit_History       564 non-null float64
+# Property_Area        614 non-null object
+# Loan_Status 614 non - null object
+
+train.describe()
+# ApplicantIncome  CoapplicantIncome  LoanAmount  Loan_Amount_Term  Credit_History
+# count       614.000000         614.000000  592.000000         600.00000      564.000000
+# mean       5403.459283        1621.245798  146.412162         342.00000        0.842199
+# std        6109.041673        2926.248369   85.587325          65.12041        0.364878
+# min         150.000000           0.000000    9.000000          12.00000        0.000000
+# 25 % 2877.500000           0.000000  100.000000         360.00000        1.000000
+# 50 % 3812.500000        1188.500000  128.000000         360.00000        1.000000
+# 75 % 5795.000000        2297.250000  168.000000         360.00000        1.000000
+# max       81000.000000       41667.000000  700.000000         480.00000        1.000000
+train['Self_Employed'].value_counts()
+
+cols = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome',
+        'CoapplicantIncome', 'LoanAmount', 'Loan_Status', 'Credit_History', 'Property_Area']
+
+train_sub = train[cols]
+
+train_sub['Gender'].fillna('Male', inplace=True)
+train_sub['Married'].fillna('Yes', inplace=True)
+train_sub['Dependents'].fillna('0', inplace=True)
+train_sub['Self_Employed'].fillna('No', inplace=True)
+train_sub['LoanAmount'].fillna(146.412162, inplace=True)
+train_sub['Credit_History'].fillna(0, inplace=True)
+
+train_sub.info()
+
+# dict_ls = {'Y': 1, 'N': 0}
+# train_sub['Loan_Status'].replace(dict_ls, inplace=True)
+
+vec = DictVectorizer()
+X_train = vec.fit_transform(train_sub.to_dict(orient='record'))
+vec.feature_names_
+print(X_train[0])
+print(len(vec.feature_names_))
+X_train.shape
+# (614, 21)
+
+# Numerics = list of all numeric fields
+# Categories = list of all categorical fields
+# Features = list of all features except the Label and Id
+
+
+def convert_to_ffm(df, type, numerics, categories, features):
+    currentcode = len(numerics)
+    catdict = {}
+    catcodes = {}
+
+    # Flagging categorical and numerical fields
+    for x in numerics:
+        catdict[x] = 0
+    for x in categories:
+        catdict[x] = 1
+
+    nrows = df.shape[0]
+    ncolumns = len(features)
+
+    with open(str(type) + "_ffm.txt", "w") as text_file:
+
+        # Looping over rows to convert each row to libffm format
+        for n, r in enumerate(range(nrows)):
+            datastring = ""
+            datarow = df.iloc[r].to_dict()
+            # Set Target Variable here
+            datastring += str(int(datarow['Loan_Status']))
+
+           # For numerical fields, we are creating a dummy field here
+            for i, x in enumerate(catdict.keys()):
+                if(catdict[x] == 0):
+                    datastring = datastring + " " + \
+                        str(i)+":" + str(i)+":" + str(datarow[x])
+                else:
+
+                   # For a new field appearing in a training example
+                    if(x not in catcodes):
+                        catcodes[x] = {}
+                        currentcode += 1
+                        # encoding the feature
+                        catcodes[x][datarow[x]] = currentcode
+
+           # For already encoded fields
+                    elif(datarow[x] not in catcodes[x]):
+                        currentcode += 1
+                        # encoding the feature
+                        catcodes[x][datarow[x]] = currentcode
+
+                    code = catcodes[x][datarow[x]]
+                    datastring = datastring + " " + \
+                        str(i)+":" + str(int(code))+":1"
+
+            datastring += '\n'
+            text_file.write(datastring)
